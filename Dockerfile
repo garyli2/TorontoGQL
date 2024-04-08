95c2b0718348c4fb363250e2fddc61ec41b5d703
Gary Li <gary.li1@uwaterloo.ca>
2024-04-08 04:44:05 +0000

    Prod deploy fixes

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM denoland/deno:latest
 # The port that your application listens to.
-EXPOSE 4000
+EXPOSE 443
 
-RUN apt update && apt install -y git
+RUN apt update && apt upgrade -y && apt install -y git
 
 WORKDIR /app
 USER root
 
 ADD . .
 RUN rm -r node_modules
-ENV SEQUELIZE_URL=postgres://postgres:abc123@db-prod:5432/postgres
+
 CMD ["run", "--allow-net", "--allow-read", "--allow-env", "--allow-run", "src/index.ts"]
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,37 @@
 import { ApolloServer } from "npm:@apollo/server";
-import { startStandaloneServer } from "npm:@apollo/server/standalone";
-import { resolve } from "https://deno.land/std@0.203.0/path/resolve.ts";
+import { expressMiddleware } from "npm:@apollo/server/express4";
+import cors from "npm:cors";
+import express from "npm:express";
+import http from "node:http";
+import https from "node:https";
 import { DateResolver } from "npm:graphql-scalars";
+import { resolve } from "https://deno.land/std@0.203.0/path/resolve.ts";
 import { Resolvers } from "./generated/graphql.ts";
 import ferryTicketCounts from "./resolvers/ferry-ticket-counts.ts";
 import bikeShareStations from "./resolvers/bike-share.ts";
-import { startBikeShareAggregator } from "./aggregator/bike-share-aggregator.ts";
 import bikeStationTrends from "./resolvers/bike-station-trends.ts";
+
+const configurations = {
+  production: { ssl: true, port: 443, hostname: "graphql.howstoronto.ca" },
+  development: { ssl: false, port: 4000, hostname: "localhost" },
+};
+
+const environment = Deno.env.get("NODE_ENV") === "production"
+  ? "production"
+  : "development";
+const config = configurations[environment];
 
 const __dirname = new URL(".", import.meta.url).pathname;
 const decoder = new TextDecoder("utf-8");
 const typeDefs = decoder.decode(
   Deno.readFileSync(resolve(__dirname, "./schema.graphql")),
 );
-
 const resolvers: Resolvers = {
   DateTime: DateResolver,
   Query: {
     ferryTicketCounts: ferryTicketCounts,
     bikeShareStations: bikeShareStations,
-    bikeStationTrends: bikeStationTrends
+    bikeStationTrends: bikeStationTrends,
   },
 };
 
@@ -27,10 +39,41 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+await server.start();
 
-// @ts-ignore weird apollo TS error
-const { url } = await startStandaloneServer(server);
-console.log(`🚀  Server ready at: ${url}`);
+const app = express();
+// our express server is mounted at /graphql
+app.use(
+  "/",
+  cors<cors.CorsRequest>(),
+  express.json(),
+  // @ts-expect-error weird TS error
+  expressMiddleware(server),
+);
 
-// start interval aggregators
-startBikeShareAggregator();
+// Create the HTTPS or HTTP server, per configuration
+let httpServer;
+if (config.ssl) {
+  httpServer = https.createServer(
+    {
+      key: decoder.decode(
+        Deno.readFileSync("/ssl/live/graphql.howstoronto.ca/privkey.pem"),
+      ),
+      cert: decoder.decode(
+        Deno.readFileSync("/ssl/live/graphql.howstoronto.ca/cert.pem"),
+      ),
+    },
+    app,
+  );
+} else {
+  httpServer = http.createServer(app);
+}
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: config.port }, resolve)
+);
+
+console.log(
+  "🚀 Server ready at",
+  `http${config.ssl ? "s" : ""}://${config.hostname}:${config.port}/`,
+);
